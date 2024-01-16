74b8fd304c90de66d233172f8af3894030368026
kanishk <138060333+kanishkrr@users.noreply.github.com>
2024-01-15 22:25:03 -0500

    meow

--- MeepMeep/src/main/java/com/example/meepmeep/Visualization.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Visualization.java
@@ -2,32 +2,38 @@ package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.profile.VelocityConstraint;
-import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
-import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.SampleMecanumDrive;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-import org.jetbrains.annotations.NotNull;
-
-public class Test {
+public class Visualization {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         Pose2d yellow = new Pose2d(46.8, 37, Math.toRadians(0));
-        Pose2d purple = new Pose2d(33, 28, Math.toRadians(180));
-        Pose2d parkRight = new Pose2d(52.2, 10, Math.toRadians(180));
-        Pose2d parkLeft = new Pose2d(47, 59.6, Math.toRadians(180));
+
+        Vector2d parkLeft = new Vector2d(55, 58.6);
+
 
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(30, 30, Math.toRadians(150), Math.toRadians(150), 17.2)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(14, 61, Math.toRadians(-90)))
-                        .lineToSplineHeading(yellow)
+                        drive.trajectorySequenceBuilder(new Pose2d(-38, 61, Math.toRadians(-90)))
+                        .strafeTo(new Vector2d(-42, 59))
+                        .lineToLinearHeading(new Pose2d(-35.3, 34.3, Math.toRadians(0)))
+                        .waitSeconds(7)
+                        .back(8)
+                        .splineToConstantHeading(new Vector2d(-21.9, 8.5), Math.toRadians(0))
+                        .forward(26)
+                        .splineToConstantHeading(new Vector2d(44.9, 36.2), Math.toRadians(0))
+                        .strafeLeft(18)
+                        .turn(Math.toRadians(180))
+                        .strafeTo(parkLeft)
                         .build()
+
 
                 );
 
